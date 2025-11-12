c934b006bd3d3126ab1ef75f1b4e044dc46422c1
Pietro <pietro.colangelo@dedagroup.it>
2025-11-12 19:46:53 +0100

    Remove SQLite migrations and switch to db push for PostgreSQL deployment

--- prisma/migrations/20251107224731_unique_shop_active_rule/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[shop,active]` on the table `DiscountRule` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "DiscountRule_shop_active_key" ON "DiscountRule"("shop", "active");
